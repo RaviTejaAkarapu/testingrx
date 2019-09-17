d0374eb44be81600ac89aa380a81431a65955a9b
Ravi teja Akarapu <ravi.teja@zopsmart.com>
2019-09-17 09:49:20 +0530

    implementing rx java and retrofit

---
+++ app/src/main/java/com/testingrx/CountryApi.kt
@@ -0,0 +1,10 @@
+package com.testingrx
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface CountryApi {
+
+    @GET("DevTides/countries/master/countriesV2.json")
+    fun getCountries(): Single<List<Country>>
+}
--- app/src/main/java/com/testingrx/ListViewModel.kt
+++ app/src/main/java/com/testingrx/ListViewModel.kt
@@ -2,8 +2,16 @@ package com.testingrx
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel: ViewModel() {
+
+    private val countryService = CountryService()
+    private val disposable = CompositeDisposable()
 
     val countries = MutableLiveData<List<Country>>()
     val countryLoadError = MutableLiveData<Boolean>()
@@ -14,21 +22,46 @@ class ListViewModel: ViewModel() {
     }
 
     private fun fetchCountries() {
-        val mockData = listOf(Country("countryA"),
-            Country("countryB"),
-            Country("countryC"),
-            Country("countryD"),
-            Country("countryE"),
-            Country("countryF"),
-            Country("countryG"),
-            Country("countryH"),
-            Country("countryI"),
-            Country("countryJ"),
-            Country("countryK")
-            )
+        loading.value = true
+        disposable.add(
+            countryService.getCountries()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object: DisposableSingleObserver<List<Country>>(){
+                    override fun onSuccess(data: List<Country>) {
+                        countries.value = data
+                        countryLoadError.value = false
+                        loading.value = false
+                    }
 
-        countryLoadError.value = false
-        loading.value = false
-        countries.value = mockData
+                    override fun onError(e: Throwable) {
+                        countryLoadError.value = true
+                        loading.value = false
+                    }
+
+                })
+        )
+
+//        val mockData = listOf(Country("countryA"),
+//            Country("countryB"),
+//            Country("countryC"),
+//            Country("countryD"),
+//            Country("countryE"),
+//            Country("countryF"),
+//            Country("countryG"),
+//            Country("countryH"),
+//            Country("countryI"),
+//            Country("countryJ"),
+//            Country("countryK")
+//            )
+//
+//        countryLoadError.value = false
+//        loading.value = false
+//        countries.value = mockData
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
